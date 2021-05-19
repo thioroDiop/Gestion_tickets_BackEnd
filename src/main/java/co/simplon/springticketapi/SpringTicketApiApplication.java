8abad98dcdc3e090b1bb4b9c4283a3bab81e4441
Jules Grand <jls.grand@gmail.com>
2021-05-19 13:50:29 +0200

    ajour du cross origin

--- src/main/java/co/simplon/springticketapi/SpringTicketApiApplication.java
+++ src/main/java/co/simplon/springticketapi/SpringTicketApiApplication.java
@@ -1,8 +1,10 @@
 package co.simplon.springticketapi;
 
-import co.simplon.springticketapi.controller.ClassroomController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
@@ -11,5 +13,15 @@ public class SpringTicketApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringTicketApiApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**").allowedOrigins("*");
+            }
+        };
     }
 }
